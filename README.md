b03b4ebde3d31969c398d4bba9ff364a188321a1
tenten0213 <takehito.0213@gmail.com>
2022-02-16 20:36:04 +0900

    画像をbase64 encodeしてjsonで返してみた

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ get image and open it.
 $ curl http://localhost:3000 -o hoge.jpeg
 $ open hoge.jpeg
 ```
+
+get json and base64 decode it
+```shell
+$ curl http://localhost:3000/json | jq -r '.image' | base64 -d > fuga.jpeg 
+$ open fuga.jpeg
+```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 #![deny(warnings)]
 
 use std::convert::Infallible;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, header, Request, Response, Server};
 
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(
-        Response::builder()
-        .header( header::CONTENT_TYPE, "application/octet-stream" )
-            .body(Body::from(include_bytes!("example.jpeg").to_vec()))
-            .unwrap()
-    )
+use hyper::{Body, header, Method, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Image {
+    pub image: String
+}
+
+async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") =>
+            Ok(
+                Response::builder()
+                    .header( header::CONTENT_TYPE, "application/octet-stream" )
+                    .body(Body::from(include_bytes!("example.jpeg").to_vec()))
+                    .unwrap()
+            ),
+        (&Method::GET, "/json") => {
+            let data = Image {
+                image: base64::encode(include_bytes!("example.jpeg"))
+            };
+            Ok(
+                Response::builder()
+                    .header(header::CONTENT_TYPE, "application/json")
+                    .body(Body::from(serde_json::to_string(&data).unwrap()))
+                    .unwrap()
+            )
+        }
+        _ => {
+            Ok(
+                Response::default()
+            )
+        }
+    }
 }
 
 #[tokio::main]
